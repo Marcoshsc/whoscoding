bd2a9a3965219d808977295e973993a760b0e8b7
Marcos Henrique <marcoshscunha@hotmail.com>
2020-01-05 15:26:45 -0300

    Pagina inicial finalizada

--- src/LoginPage/form.js
+++ src/LoginPage/form.js
@@ -1,14 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import './formStyle.css';
+import '../mainPage/indexMainPage.css';
 
 function Form(props) {
     let [username, setUsername] = useState("");
+    let [password, setPassword] = useState("");
 
-    function handleChange()
+    function handleChange(e, callback) {
+        callback(e.target.value);
+    }
+
+    function handleSubmit() {
+
+    }
 
     return (
-        <form>
-            <input type="text" value={username} onChange={handleUsernameChange} placeholder="User Name"/>
-        </form>
+        <div id="wrapper">
+            <form onSubmit={handleSubmit} className="login-form">
+                <div id="inputs">
+                    <input type="text" value={username} onChange={(e) => handleChange(e, setUsername)} placeholder="User Name"/>
+                    <input type="password" value={password} onChange={(e) => handleChange(e, setPassword)} placeHolder="Password"/>
+                </div>
+                <div id="buttons" className={"form-buttons"}>
+                    <button id="submit-button" type="submit" value="submit">Login</button>
+                    <button id="sign-up-button">Sign up</button> 
+                    <button id="forgot-my-password-button">Forgot My Password</button>
+                </div>
+            </form>
+        </div>
     )
 }
 
--- src/LoginPage/index.js
+++ src/LoginPage/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import From from './form.js';
+import Form from './form.js';
+import './loginPageStyle.css';
+import '../mainPage/indexMainPage.css';
+import '../aboutPage/indexAboutPage.css';
 
 function LoginPage(props) {
     return (
-        <div id={"login-page"}>
+        <div id={"login-page"} className={"about-page"}>
             <button onClick={props.back}>Return Home</button>
+            <h1>Login to Who's Coding</h1>
             <Form/>
         </div>
     )
